48a5eb72781f2ce69688e49c5008bc4b4adc8779
nimitzshah <nimitzshah@gmail.com>
2015-03-11 20:56:26 -0400

    Add Enormous input problem

---
+++ enormous_input.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "bufio"
+import "os"
+import "strconv"
+
+func main() {
+  var divisor int;
+  var inputs int;
+  var total int;
+  total =0;
+  fmt.Scanf("%d %d",&inputs, &divisor)
+  scanner := bufio.NewReader(os.Stdin)
+  for input:= 0;input <inputs;input++ {
+    input_string,_,_ := scanner.ReadLine()
+    dividend,_:= strconv.Atoi(string(input_string))
+    if(dividend%divisor ==0){
+      total= total +1
+    }
+  }
+  fmt.Println(total)
+}
